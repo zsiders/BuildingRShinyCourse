7fe4706f2a6b31dc4b601e9c66f20967515ddaec
Zach Siders <zachary.siders@gmail.com>
2025-05-28 11:37:59 -0400

    add prerender

---
+++ Day 4 - Advanced UI/prerenderMd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+#pre-render Markdown
+render('/Users/zach/Dropbox/Teaching/2025 - FAS 6932 - RShiny/BuildingRShinyCourse/README.md', output_format='html_document',output_file="/Users/zach/Dropbox/Teaching/2025 - FAS 6932 - RShiny/BuildingRShinyCourse/README.html")
